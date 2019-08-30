2a6ebb48ce5a2d30e6a446497900e0ee612d5f44
jessilee <jessilee@192.168.2.5>
2019-08-29 21:27:33 -0400

    Added the finishing code, such as if-else states to compare numbers, avg total price, etc.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -14,41 +14,134 @@ public class A1Adept {
 		
 		int inventory = scan.nextInt();
 		
-		// A for loop to run through each item in the store:
+		// An array declaration to store the item and price, & for
+		// loop to run through the input item info:
+		
+		String[] item = new String[inventory];
+		double[] price = new double[inventory];
 		
 		for (int i=0; i<inventory; i++) {
 	  		String inventoryItem = scan.next();
+	  		item[i] = inventoryItem;
 	  			
 	 		double inventoryPrice = scan.nextDouble();
-	 	//		double[] price = new double[inventoryPrice];
+	 		price[i] = inventoryPrice;
+	 	
 		}	
 		
-		// declaration of the number of customers:
+		// integer count of the number of customers:
 		
 	 	int numCustomers = scan.nextInt();
 	 	
-	 	// for loop to run through customer-related data (ex names and purchases):
+	 	// Arrays to store names & totals data regarding customer purchases:
+	 	
+	 	String[] firstname = new String[numCustomers];
+	 	String[] lastname = new String[numCustomers];
+	 	
+ 		double cost = 0; // initialized to keep track of individual total prices
+ 		double[] totals = new double[numCustomers];
+ 		
+// 		double average = avg(totals);
 	 	
 	 	for (int j=0; j<numCustomers; j++) {
+	 		
 	 		String first = scan.next();
+	 		firstname[j] = first;
+	 		
 	 		String last = scan.next();
+	 		lastname[j] = last;
 	 		
 	 		int totalItemsBought = scan.nextInt();
-	 	
-		 		for (int p=0; p<totalItemsBought; p++) {
+	 		
+	 		double[] register = new double[totalItemsBought]; 
+	 		
+	 		// for loop to run through customer purchases:
+	 		
+		 		for (int k=0; k<totalItemsBought; k++) {
+		 			
 		 			int itemQuantity = scan.nextInt();
-		 				int[] quantity = new int[itemQuantity];
-		 			double itemPrice = scan.nextDouble();
-		 	//		String item = scan.next();
-		 		
-	 			}
-		 		
-		 	
+		 			String itemInCart = scan.next();
+		 			
+			 	// another for loop to find purchased item in inventory array:
+			 		for (int l = 0; l < inventory; l++) { 
+
+			 			if (itemInCart.equals(item[l])) {
+			 				
+			 				cost = price[l]*itemQuantity;
+			 				register[k] = cost;
+			 			}
+			 		}
+
+		 	double totalCost = 0;
+		 	for (int m=0; m<totalItemsBought; m++) {
+		 		totalCost += register[m];
+		 		totals[j] = totalCost;
+		 	}
+			 	
 	 	}
+	
+//	 	scan.close(); 
 	 	
+		 
+//		 double average = avg(totals);
+//		 System.out.println("Average: " + average); 
+		 
+	 }
 	 	
+	 	// set variables and started for loop to compare totals:
+ 		
+	 	double smaller = totals[0];
+	 	double bigger = 0;
+//	 	smaller = totals[0];
+	 	String biggest = "";
+	 	String smallest = "";
+		 	
+		 for (int n = 0; n < totals.length; n++) {
+			
+			if (totals[n] > bigger) {
+				bigger = totals[n];	
+				biggest = firstname[n] + " " + lastname[n];
+				 
+	 		} else {
+	 			if (totals[n] < smaller) {
+	 				smaller = totals[n];
+	 				smallest = firstname[n] + " " + lastname[n];
+	 				
+	 			}
+	 		}
+	 	
+		 }
 		
+		double add = 0;
+		double avg = add;
 		
+		for (int o=0; o<totals.length; o++) {
+			add += totals[o];
+		}
 		
-	}
+		avg = add / totals.length;
+		
+		System.out.println("Biggest: " + biggest + " (" + bigger + ")");
+		System.out.println("Smallest: " + smallest + " (" + smaller + ")");
+	 	System.out.println("Average: " + String.format("%.2f", avg));	
+	 
+	 }
+	
+//	static double avg (double[] totals) {
+//		
+//		double add = 0;
+//		double hehe = 0;
+//		
+//		for (int o = 0; o < totals.length; o++) {
+//			
+//			add += totals[o];
+//			
+//		}
+//		
+//		hehe = add / totals.length;
+//		System.out.println(add);
+//		return hehe;
+//	}
+	 
+		
 }
