1a128fdc3fa8c5690c286da1c73629a4db40253b
jessilee <jessilee@jessilee-pc.wireless-1x.unc.edu>
2019-08-30 16:24:43 -0400

    Coded for loop to match items with specific quantities and to count the number of buyers.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,26 +9,85 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// An integer count of the number of items in the store:
 		
 		int inventory = scan.nextInt();
 		
+		// An array declaration to store the item and price, & a 
+		// for loop to run through the input item info:
+		
+		String[] item = new String[inventory];
+		
 		for (int i=0; i<inventory; i++) {
-			String inventoryItem = scan.next();
-			double inventoryPrice = scan.nextDouble();
-		}
-		
-		int numCustomers = scan.nextInt();
-		
-		for (int j=0; j<numCustomers; j++) {
-			String first = scan.next();
-			String last = scan.next();
 			
-			int numItems = scan.nextInt();
-			
-				for (int k=0; k<numItems; k++) {
-					int itemQuantity = scan.nextInt();
-					String item = scan.next();
-				}
-		}
+	  		String inventoryItem = scan.next();
+	  		item[i] = inventoryItem;
+	  		
+	  		Double inventoryPrice = scan.nextDouble();
+	 	
+		}	
+		
+		// integer count of the number of customers:
+		
+	 	int numCustomers = scan.nextInt();
+	 	int[] whatQuantity = new int[inventory];
+	 	int[] customerTotals = new int[inventory];
+
+	 	// for loop to scan through customer data:
+ 		
+	 	for (int j=0; j<numCustomers; j++) {
+	 		
+	 		String first = scan.next();
+	 		String last = scan.next();
+	 		
+	 		int totalItemsBought = scan.nextInt();
+	 		
+	 		for (int k=0; k<totalItemsBought; k++) {
+	 			
+	 			int quantityBought = scan.nextInt();
+	 			
+	 			String itemBought = scan.next();
+
+	 		
+	 			
+	 			for (int l=0; l<inventory; l++) {
+	 				
+	 				if (itemBought.equals(item[l])) {
+	 					whatQuantity[l] += quantityBought;
+	 					customerTotals[l] += 1;
+	 				}
+	 			}
+	 			
+	 			
+	 		}
+	 		
+//	 		for (int m=0; m<inventory; m++) {
+//	 			
+//	 			if (customerTotals[m] == 0) {
+//	 				System.out.println("No customers bought " + item[m]);
+//	 				
+//	 			} else {
+//	 				System.out.println(customerTotals[m] + " customers bought " + whatQuantity + item[m]);
+//	 			}
+//	 			
+//	 		
+//	 			
+//	 		}
+	 		
+	
+	 	}
+	 	
+ 		for (int m=0; m<inventory; m++) {
+ 			
+ 			if (customerTotals[m] == 0) {
+ 				System.out.println("No customers bought " + item[m]);
+ 				
+ 			} else {
+ 				System.out.println(customerTotals[m] + " customers bought " + whatQuantity[m]+ " " + item[m]);
+ 			}
+ 			
+ 		
+ 			
+ 		}
 	}
 }
